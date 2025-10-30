d3a608d060ab3f97a8c20a1bef6e2d9e3384814e
Kwa2003 <kawakiwoamekuhc@Gmail.com>
2025-10-30 20:19:27 +0800

    Update lane.py

--- Lane_Detection_v2/utils/lane.py
+++ Lane_Detection_v2/utils/lane.py
@@ -18,6 +18,10 @@ def pred2coords(pred, row_anchor, col_anchor, local_width=1, original_image_widt
     row_lane_idx = [1, 2]
     col_lane_idx = [0, 3]
 
+    L2lanes_points = []
+    L1lanes_points = []
+    R1lanes_points = []
+    R2lanes_points = []
     for i in row_lane_idx:
         tmp = []
         if valid_row[0, :, i].sum() > num_cls_row / 2:
@@ -30,7 +34,13 @@ def pred2coords(pred, row_anchor, col_anchor, local_width=1, original_image_widt
                     out_tmp = (pred['loc_row'][0, all_ind, k, i].softmax(0) * all_ind.float()).sum() + 0.5
                     out_tmp = out_tmp / (num_grid_row - 1) * original_image_width
                     tmp.append((int(out_tmp), int(row_anchor[k] * original_image_height)))
-            coords.append(tmp)
+            if len(tmp) > 2:
+                if i == 1 :
+                    L1lanes_points.append(tmp)
+                else:
+                    R1lanes_points.append(tmp)
+    if R1lanes_points:
+        R1lanes_points[0].reverse()
 
     for i in col_lane_idx:
         tmp = []
@@ -44,6 +54,12 @@ def pred2coords(pred, row_anchor, col_anchor, local_width=1, original_image_widt
                     out_tmp = (pred['loc_col'][0, all_ind, k, i].softmax(0) * all_ind.float()).sum() + 0.5
                     out_tmp = out_tmp / (num_grid_col - 1) * original_image_height
                     tmp.append((int(col_anchor[k] * original_image_width), int(out_tmp)))
-            coords.append(tmp)
+            if len(tmp) > 2:
+                if i == 0 :
+                    L2lanes_points.append(tmp)
+                else:
+                    R2lanes_points.append(tmp)
+    if R2lanes_points:
+        R2lanes_points[0].reverse()
 
-    return coords
+    return L2lanes_points ,L1lanes_points, R1lanes_points, R2lanes_points
